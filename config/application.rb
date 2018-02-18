800f15b20e1fc5eff05c403153361c9bb8b362e8
dlinch <dlinch33@gmail.com>
2018-02-17 19:35:00 -0700

    removed app/assets, removed generators from application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,14 @@ module EvilChat
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.channel         assets: false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
